bd0e11147525fd00fc6f8d37189e8d9a375c4b5e
Radyah2001 <haydar919@gmail.com>
2023-01-05 14:51:50 +0100

    Added pics Changed background color Title text is now white

--- app/src/main/java/group4/sensimate/data/repository/EventRepository.kt
+++ app/src/main/java/group4/sensimate/data/repository/EventRepository.kt
@@ -9,12 +9,12 @@ import group4.sensimate.data.model.Survey
 
 class EventRepository(){
     private var events= mutableListOf<Event>(
-        Event(1, R.drawable.ic_launcher_background, "wine", "lets celebrate", "1-1-22", "2-1-22"),
-        Event(2, R.drawable.ic_launcher_background, "food", "lets celebrate", "1-1-22", "2-1-22"),
-        Event(3, R.drawable.ic_launcher_background, "gril", "lets celebrate", "1-1-22", "2-1-22"),
-        Event(4, R.drawable.ic_launcher_background, "sandwich", "lets celebrate", "1-1-22", "2-1-22"),
-        Event(5, R.drawable.ic_launcher_background, "drum", "lets celebrate", "1-1-22", "2-1-22"),
-        Event(6, R.drawable.ic_launcher_background, "cola", "lets celebrate", "1-1-22", "2-1-22")
+        Event(1, R.drawable.wine, "Wine", "lets celebrate", "1-1-22", "2-1-22"),
+        Event(2, R.drawable.beer, "Beer", "lets celebrate", "1-1-22", "2-1-22"),
+        Event(3, R.drawable.champagne, "Champagne", "lets celebrate", "1-1-22", "2-1-22"),
+        Event(4, R.drawable.sandwich, "Sandwich", "lets celebrate", "1-1-22", "2-1-22"),
+        Event(5, R.drawable.catering, "Catering", "lets celebrate", "1-1-22", "2-1-22"),
+        Event(6, R.drawable.cake, "Cake", "lets celebrate", "1-1-22", "2-1-22")
     )
     fun getEvents(): List<Event>{
         return  events
